fc5dc1a4f2c5230687a5f5dfb909c0d7089a3037
Farhan matar <farhan.alafi@gmail.com>
2019-06-13 10:12:19 +0200

    upup

---
+++ Javascript/varibles/varibles.js
@@ -0,0 +1,44 @@
+let firstName,LastName;
+firstName='Farhan';
+let linebreak="<br>";
+
+// alert(firstName);
+LastName='allafi'
+console.log("My name is:" + firstName+ " " + LastName);
+console.log(typeof(firstName));
+
+let x=10;
+// document.write(x%4);
+document.write(x*5);
+document.write('<br>');
+document.write(x/5);
+// document.write('<div style="background:red;height:250px;"> new div </div>');
+document.write(linebreak);
+let y= x+ 'string text ';
+document.write(y);
+x=5;
+document.write(linebreak);
+document.write('x = '); 
+document.write(x); 
+result=x--;
+
+document.write(linebreak);
+document.write('x-- = '); 
+document.write(result);
+document.write(linebreak);
+document.write('result = ');
+document.write(result);
+// compration oprater 
+document.write(linebreak);
+let age=17;
+let minAge=18;
+document.write(age==minAge);
+
+if(age==minAge){
+    alert('you are ' + age );
+}
+document.write(linebreak);
+document.write(age >=minAge);
+if(age==minAge);{
+alert('you are ' + minage + 'or older' );
+}
