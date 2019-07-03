f827b187e6787cafcbc8f2209335bdc1db7c4415
Farhan matar <farhan.alafi@gmail.com>
2019-07-03 16:10:55 +0200

    hh

--- JAVASCRIPT FOLDER/date-Math-Color/random-color.js
+++ JAVASCRIPT FOLDER/date-Math-Color/random-color.js
@@ -1,4 +1,17 @@
-function getRandomcolor(){
-    let letters="0123456789ABCDEF";
-    
+let p = document.getElementById('colorCode');
+let div = document.getElementById('myColor');
+
+
+function getRandomColor() {
+    let letters = "0123456789ABCDEF";
+    let color = "#";
+    for (let i = 0; i < 6; i++) {
+
+        color += letters[Math.floor(Math.random() * 16)];
+
+    }
+
+    p.innerHTML = color;
+    div.style.background = color;
+
 }
